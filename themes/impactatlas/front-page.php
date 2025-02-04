26ecff628334fff287d3f47c0a261a47ba8eaa39
Jessica-Pelletier <jj.pelletier@outlook.com>
2025-02-04 14:24:04 -0800

    Create Join and featured template parts, Create ACF fields for join

--- themes/impactatlas/front-page.php
+++ themes/impactatlas/front-page.php
@@ -8,6 +8,8 @@ get_header();
 ?>
 
 <?php get_template_part('template-parts/home-page/hero'); ?>
+<?php get_template_part('template-parts/join') ?>
+<?php get_template_part('template-parts/featured') ?>
 
 
 
---
+++ themes/impactatlas/template-parts/join.php
@@ -0,0 +1,29 @@
+<?php $joinheading = get_field('join_heading'); ?>
+<?php $joinbuttontext = get_field('join_button_text'); ?>
+<?php $joinparagraph = get_field('join_paragraph'); ?>
+
+
+
+
+
+
+<div class="container pt-4">
+
+  <div class="row justify-content-center mb-4">
+    <div class="col-12 text-center">
+      <h2><?php echo $joinheading;?></h2>
+    </div>
+  </div>
+
+
+  <div class="row">
+
+    <div class="col-md-6">
+      <p><?php echo $joinparagraph;?></p>
+    </div>
+
+    <div class="col-md-6 text-center">
+      <button class="btn btn-primary"><?php echo $joinbuttontext;?></button>
+    </div>
+  </div>
+</div>
