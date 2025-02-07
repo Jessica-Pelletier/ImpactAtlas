44ac84c94e7f37ba68e5f54b28ec5f4dec7babe8
Jessica-Pelletier <jj.pelletier@outlook.com>
2025-02-07 12:23:04 -0800

    Create template parts and call them in about page

--- themes/impactatlas/about.php
+++ themes/impactatlas/about.php
@@ -7,4 +7,11 @@ Template Name:  about
 get_header(); ?>
 
 
+
+<?php get_template_part('template-parts/about/who_we_are')?>
+<?php get_template_part('template-parts/about/key_values')?>
+<?php get_template_part('template-parts/about/our_approach')?>
+<?php get_template_part('template-parts/about/who_partners')?>
+
+
 <?php get_footer(); ?>
