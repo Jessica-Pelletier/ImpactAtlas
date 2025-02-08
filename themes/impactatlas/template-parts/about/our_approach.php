d6ca3fbbcd34269787ded700051f41812cad3eac
Jessica-Pelletier <jj.pelletier@outlook.com>
2025-02-08 14:54:09 -0800

    Add body copy and make the text center

--- themes/impactatlas/template-parts/about/our_approach.php
+++ themes/impactatlas/template-parts/about/our_approach.php
@@ -8,35 +8,35 @@
     </div>
 
 
-    <div class="row align-items-center mb-5">
-      <div class="col-md-6">
+    <div class="row align-items-center  mb-5">
+      <div class="col-md-6 text-center">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/placeholder.jpg" alt="Description" class="img-fluid w-50">
       </div>
-      <div class="col-md-6">
-        <h3>Step 1</h3>
-        <p></p>
+      <div class="col-md-6 text-center">
+        <h3>Step 1 - Document </h3>
+        <p>Our process begins with finding and documenting successful solutions and initiatives. We then create a detailed analysis of what worked and why.</p>
       </div>
     </div>
 
 
     <div class="row align-items-center mb-5">
-      <div class="col-md-6">
-        <h3>Step 2</h3>
-        <p></p>
+      <div class="col-md-6 text-center">
+        <h3>Step 2 - Analyze</h3>
+        <p class="container-sm">We break down success patterns and verify their impacts. We work at creating replicable frameworks from the data.</p>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 text-center">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/placeholder.jpg" alt="Description" class="img-fluid w-50">
       </div>
     </div>
 
 
-    <div class="row align-items-center">
-      <div class="col-md-6">
+    <div class="row align-items-center mb-5">
+      <div class="col-md-6 text-center">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/placeholder.jpg" alt="Description" class="img-fluid w-50">
       </div>
-      <div class="col-md-6">
-        <h3>Step 3</h3>
-        <p></p>
+      <div class="col-md-6 text-center">
+        <h3>Step 3 - Share</h3>
+        <p>We beleive in making solutionals accessible by creating clear documentation and connecting with change makers. Enabling implementations of our findings elsewhere.</p>
       </div>
     </div>
 
---
+++ themes/impactatlas/template-parts/about/partners.php
@@ -0,0 +1,16 @@
+<section>
+    <div class="grid text-center">
+<div class="g-col-3"> g-col-3</div>
+<div class="g-col-3"> g-col-3</div>
+<div class="g-col-3"> g-col-3</div>
+<div class="g-col-3"> g-col-3</div>
+<div class="g-col-3"> g-col-3</div>
+<div class="g-col-3"> g-col-3</div>
+<div class="g-col-3"> g-col-3</div>
+<div class="g-col-3"> g-col-3</div>
+
+
+
+
+    </div>
+</section>
