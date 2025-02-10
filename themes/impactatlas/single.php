b5eb7186209dc8e6b1a47143ca4271bd743ccfa9
Jessica-Pelletier <jj.pelletier@outlook.com>
2025-02-10 13:45:51 -0800

    Make featured articles show featured articles with an ACF field

--- themes/impactatlas/single.php
+++ themes/impactatlas/single.php
@@ -12,6 +12,10 @@ get_header();
 
 	<main id="primary" class="site-main">
 
+
+
+
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -25,6 +29,11 @@ get_header();
 				)
 			);
 
+
+
+
+
+	
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
--- themes/impactatlas/template-parts/featured.php
+++ themes/impactatlas/template-parts/featured.php
@@ -1,46 +1,59 @@
-<section class="featured bg-light"><div class="container pt-5 pb-5">
-  <h2 >Featured Articles</h2>
+<section class="featured bg-light">
+    <div class="container pt-5 pb-5">
+        <h2>Featured Articles</h2>
+        <div class="row justify-content-center gy-4 g-md-4">
+            <?php 
+            $featured_args = array(
+                'post_type' => 'articles',
+                'posts_per_page' => 3,
+                'meta_query' => array(
+                    array(
+                        'key' => 'featured_article', 
+                        'value' => '1',
+                        'compare' => '='
+                    )
+                )
+            );
+
+            $featured_query = new WP_Query($featured_args);
+
+            if($featured_query->have_posts()) : 
+                while($featured_query->have_posts()) : $featured_query->the_post(); 
+                ?>
+                <div class="col-lg-4 col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5><?php the_title()?></h5>
+                            <h6 class="card-subtitle mb-2 secondary">
+                                <?php 
+                                $terms = get_the_terms(get_the_ID(), 'article_categories');
+                                if ($terms && !is_wp_error($terms)) {
+                                    $first_term = reset($terms);
+                                    echo $first_term->name;
+                                }
+                                ?>
+                            </h6>
+                            <p class="card-text">
+                                <?php echo wp_trim_words(get_field('what'), 20, '...'); ?>
+                            </p>
+                            <a href="<?php the_permalink(); ?>" class="card-link">View Study</a>
+                        </div>
+                    </div>
+                </div>
+                <?php 
+                endwhile;
+            endif; 
+            wp_reset_postdata();
+            ?>
+        </div>
+    </div>
+</section>
 
 
-  <div class="row justify-content-center gy-4 g-md-4">
     
 
-    <div class="col-lg-4 col-md-4">
-      <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-4 col-md-4">
-      <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
-    </div>
 
 
-    <div class="col-lg-4 col-md-4">
-      <div class="card" >
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
-    </div>
 
   </div>
 </div>
