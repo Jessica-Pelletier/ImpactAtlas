23368e116af09608748d0be0b40802580e7660a3
Jessica-Pelletier <jj.pelletier@outlook.com>
2025-02-11 13:41:45 -0800

    Align view article link to the right of article

--- themes/impactatlas/single-articles.php
+++ themes/impactatlas/single-articles.php
@@ -28,7 +28,7 @@ get_header(); ?>
 					</header>
 
 					<!-- Article Content -->
-					<div class="article-content">
+					<div class="article-content d-flex flex-column">
 						<!-- Get your ACF fields -->
 						<?php if (get_field('what')): ?>
 							<div class="section mb-4">
@@ -58,7 +58,7 @@ get_header(); ?>
 						<?php endif; ?>
 
 						<?php if (get_field('original_link')); ?>
-						<a  href="<?php echo get_field('original_link') ?>">View Study <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+						<a  class="align-self-end" href="<?php echo get_field('original_link') ?>">View Study <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
 							</svg></a>
 					</div>
