abf93d828063673c434a82a422a13a3f90179059
Jessica-Pelletier <jj.pelletier@outlook.com>
2025-02-19 12:06:32 -0800

    Add basics of leaflet pop ups and markers

--- themes/impactatlas/js/main.js
+++ themes/impactatlas/js/main.js
@@ -1,6 +1,26 @@
-const map = L.map( 'map' ).setView( [ 0, -0 ], 10 );
+const map = L.map( 'map' ).setView( [ 0, -0 ], 1 );
 
 L.tileLayer( 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 1,
+	maxZoom: 15,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 } ).addTo( map );
+
+//POPup
+
+const popup = L.popup();
+let marker;
+
+function onMapClick( e ) {
+	popup
+		.setLatLng( e.latlng )
+		.setContent( 'You clicked the map at ' + e.latlng.toString() )
+		.openOn( map );
+
+	// if ( marker ) {
+	// 	map.removeLayer( marker );
+	// }
+
+	// marker = L.marker( e.latlng ).addTo( map );
+}
+
+map.on( 'click', onMapClick );
