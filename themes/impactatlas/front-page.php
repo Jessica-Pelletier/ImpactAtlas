0fb34ed527d37127b9eead67231958622938362c
Jessica-Pelletier <jj.pelletier@outlook.com>
2025-02-23 13:42:10 -0800

    Add a sub heading and description to the progress map

--- themes/impactatlas/front-page.php
+++ themes/impactatlas/front-page.php
@@ -10,62 +10,66 @@ get_header();
 <?php get_template_part('template-parts/home-page/banner') ?>
 <!--leaflet.js map section-->
 
+    <div class="container text-center">
 
-
-
-
-
-
-
-<div class="container text-center">
-
-    <div class="row">
-        <div class="col-md-12 pt-4">
-            <div>
-                <h2>A Year of global impact</h2>
-            </div>
-        </div>
-        <div class="col">
-
-            <div id="map"></div>
-        </div>
-
-
-    </div>
-
-
-    <div class="col">
         <div class="row">
-            <div class="row py-2">
-                <h3>Legend</h3>
+            <div class="col-md-12 pt-4">
+                <div>
+                    <h2>A Year of global impact</h2>
+                </div>
             </div>
-            <div class="row py-2">
-                <p>2024 was a year of growth, innovation, and meaningful impact </p>
+            <div class="col">
+
+                <div id="map"></div>
             </div>
 
-            <div class="row py-2">
-                <div class="col-6" style="background-color: #2ecc71"></div>
-                <div class="col-6">Environmental</div>
-            </div>
-            <div class="row py-2">
-                <div class="col-6" style="background-color: #e74c3c"></div>
-                <div class="col-6">Technology</div>
-            </div>
-            <div class="row py-2">
-                <div class="col-6" style="background-color: #DF7D3B"></div>
-                <div class="col-6">Social</div>
-            </div>
-            <div class="row py-2">
-                <div class="col-6" style="background-color: #34495e"></div>
-                <div class="col-6">Economic</div>
-            </div>
-            <div class="row py-2">
-                <div class="col-6" style="background-color: #9b59b6"></div>
-                <div class="col-6">Success Stories</div>
-            </div>
+
         </div>
+
+
     </div>
-</div>
+
+
+<section class="map-info py-4">
+    <div class="container">
+        <div class="col-lg-8">
+            <h2> Tracking Global Progress in 2024 </h2>
+            <p>Our map documents verfified solutions and positive initiatives completed throughout 2024, creating an atlas of global progress.</p>
+            <p>Each marker represents a documented success story - from environmental conservation to social innovation. By mapping these solutions as they happen, we're building a resource of what works, where, and how it can be replicated.</p>
+        </div>
+        <div class="col-lg-4">
+            <div class="row g-3">
+                <div class="col-6">
+                    <div class="p-3 bg-light rounded text-center">
+                        <div class="h2 mb-1">42</div>
+                        <p class="text-muted">Countries</small>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="p-3 bg-light rounded text-center">
+                        <div class="h2 mb-1">275</div>
+                        <p class="text-muted">Solutions</small>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
 
 
 
